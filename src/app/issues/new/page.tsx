88c474d9a844b930423dd45c078bd42d77701e11
yuan <2284810742@qq.com>
2024-12-21 14:00:06 +0800

    new 提交表单样式编写 未实现提交功能

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -1,18 +1,21 @@
 "use client";
 import { Button, TextArea, TextField } from "@radix-ui/themes";
 import dynamic from "next/dynamic";
+
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
     ssr: false,
   });
 const NewIssuePage = () => {
   return (
     <div className="max-w-xl space-y-3">
-<TextField.Root>
-  
-</TextField.Root>
+      <TextField.Root placeholder="title">
+        <TextField.Slot>
+        </TextField.Slot>
+      </TextField.Root>
       <TextArea placeholder="Description" />
       <Button>Submit New</Button>
     </div>
+
   );
 };
 export default NewIssuePage;
