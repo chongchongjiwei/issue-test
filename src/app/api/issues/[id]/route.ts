8a425a5dcd4a027ef810c79d2f0cbb2027dcfbce
yuan <2284810742@qq.com>
2024-12-23 15:49:43 +0800

    构建用于更新的API，实现更新功能

---
+++ src/app/api/issues/[id]/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+import { issueSchema } from "./validationSchema";
+import prisma from "@/app/client";
+import { describe } from "node:test";
+
+interface Props{
+    params:{params:{id:string}}
+}
+
+
+export async function PATCH(request:NextRequest,{params}:{params:{id:string}})
+{
+    const body = await request.json();
+    const validation= issueSchema.safeParse(body);
+    if(!validation.success)
+        return NextResponse.json(validation.error.format(),{status:400});
+
+    const issue =await prisma.issue.findUnique({
+        where:{id:parseInt(params.id)}
+    })
+    if(!issue)
+        return NextResponse.json({'error':"Invalid issue"},{status:404});
+    const updateResult = await prisma.issue.update({
+        where: { id: issue.id }, // 修复语法错误
+        data: {
+            title: body.title,        // 确保 body 已定义
+            description: body.description, // 确保字段拼写正确
+        },
+    });
+    return NextResponse.json(updateResult);
+
+}
