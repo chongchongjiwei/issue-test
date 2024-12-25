4dbef39362ea6751e5ca407dc4f4df7abfe1b035
yuan <2284810742@qq.com>
2024-12-25 15:21:44 +0800

    添加删除弹出对话框

--- src/app/issues/[id]/DeleteIssueButton.tsx
+++ src/app/issues/[id]/DeleteIssueButton.tsx
@@ -1,9 +1,27 @@
-import { Button } from '@radix-ui/themes'
+'use client'
+import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 import React from 'react'
 
 const DeleteIssueButton = ({issueId}:{issueId:number}) => {
   return (
-    <Button color='red'>Delete Issue</Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+      <Button color='red'>Delete Issue</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title> 确认删除</AlertDialog.Title>
+        <AlertDialog.Description>你确认要删除这个数据吗？</AlertDialog.Description>
+        <Flex mt ='4' gap='3'>
+          <AlertDialog.Cancel>
+            <Button variant='soft' color='gray'>取消</Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color='red'>确认</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+
+    </AlertDialog.Root>
   )
 }
 
